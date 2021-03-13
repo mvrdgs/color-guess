74b01585632ace68f309704cf18984ed45f27814
Viegas <mvrdgs@gmail.com>
2021-03-13 20:28:17 -0300

    requisito 5 e quase todo 6

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 const circles = document.querySelectorAll('.ball');
+const optionsContainer = document.querySelector('#options-container');
+const rightAnswerColor = document.querySelector('#rgb-color');
+const answerText = document.querySelector('#answer-text');
+const resetButton = document.querySelector('#reset-game');
+let gameAnswer;
+
+function resetGame() {
+  chooseAnswer();
+  setRandomColors();
+  setRightAnswer();
+  document.querySelector('#selectAnswer').id = '';
+  answerText.innerText = '';
+}
+
+function verifyAnwser() {
+  const selectedAnwser = document.querySelector('#selectAnswer');
+  if (selectedAnwser.style.backgroundColor === gameAnswer.style.backgroundColor) {
+    answerText.innerText = 'Acertou!';
+  } else {
+    answerText.innerText = 'Errou! Tente novamente!'
+  }
+}
+
+function setRightAnswer() {
+  const rightAnswer = circles[Math.floor((Math.random()*circles.length))];
+  rightAnswerColor.innerHTML = rightAnswer.style.backgroundColor;
+  gameAnswer = rightAnswer;
+}
+
+function chooseAnswer() {
+  optionsContainer.addEventListener('click', () => {
+    const answer = event.target;
+    const answerLimiter = document.querySelector('#selectAnswer');
+    if (answerLimiter === null && answer.classList.contains('ball')) {
+      answer.id = 'selectAnswer';
+      verifyAnwser();
+    }
+  });
+}
 
 function generateRandomColor() {
   const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
@@ -12,9 +51,11 @@ function generateRandomColor() {
 
 function setRandomColors() {
   for (let index = 0; index < circles.length; index += 1) {
-    console.log(circles[index]);
     circles[index].style.backgroundColor = generateRandomColor();
   }
 }
 
+resetButton.addEventListener('click', resetGame);
+chooseAnswer();
 setRandomColors();
+setRightAnswer();
